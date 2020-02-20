7371ec696ad901d91f3007d9b3559057596541a9
Jon Olson <JOlson1530@hotmail.com>
2020-02-20 16:59:04 -0500

    first commit

---
+++ cart.js
@@ -0,0 +1,20 @@
+let cartArray = [
+    {id:  1,
+    product: 'Apple',
+    price: 2.50,
+    quantity: 10},
+    {id:  2,
+    product: 'Grape',
+    price: 3.50,
+    quantity: 50},
+    {id:  3,
+    product: 'Pineapple',
+    price: 4.50,
+    quantity: 1},
+    {id:  4,
+    product: 'Strawberry',
+    price: 3,
+    quantity: 16},
+];
+
+module.exports = { cartArray };
---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const cart = require('./cart');
+const cors = require('cors');
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 
+  };
+
+const app = express();
+const port = 3000;
+
+const cartArray = cart.cartArray;
+
+app.use(express.json());
+app.options('/cart-items/:id', cors()) 
+
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`)
+});
+
+app.get( '/cart-items',  cors(corsOptions), (req, res) => {
+    res.json(cartArray);
+});
+
+app.get( '/cart-items/:id',  cors(corsOptions),(req, res) => {
+    res.status(200);
+    res.json(cartArray[req.params.index]);
+});
+
+app.post( '/cart-items',  cors(corsOptions),(req, res) => {
+    res.status(200);
+    const body = req.body.data;
+    cartArray.push(body);
+    res.json("Cart item successfully added");
+});
+
+app.put( '/cart-items/:id',  cors(corsOptions), (req, res) => {
+    res.status(201);
+    const index = req.params.index;
+    const body = req.body.data;
+
+    const newCart = {
+        id: ID,
+        product: body.product,
+        price: body.price,
+        quantity: body.quantity
+    };
+    
+    cartArray.splice(index, 1, newCart)
+    res.json("Updating cart item by id");
+});
+
+app.delete( '/cart-items/:id',  cors(corsOptions), (req, res) => {
+    res.status(204);
+    const index = req.params.index;
+    cartArray.splice(index, 1)
+    res.status(204).json("Deleting cart item");
+});
